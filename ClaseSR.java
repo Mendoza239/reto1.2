8af80c613dd16a24abf4f5e9f442f3c7a2f2f38d
Ismael Mendoza <ismaelmendoza768@gmail.com>
2022-07-08 16:39:15 -0500

    arreglado el error

--- ClaseSR.java
+++ ClaseSR.java
@@ -1,3 +1,9 @@
 public class ClaseSR {
-    
+    public Double suma(Double num1, Double num2){
+        return (num1+num2);
+    }
+
+    public Double resta(Double num1, Double num2){
+        return (num1-num2);
+    }
 }
